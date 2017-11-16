7e14fd37e02bfe75efc486580c99b29eb3f7eed4
Peter Tsai <hyptsai@gmail.com>
2017-11-16 21:10:48 +0200

    Deleted all files from the tutorials

--- 2.conditionals.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-a, b = 0, 1
-if a < b:
-    print('a ({}) is less than b ({})'.format(a, b)) # The {} is replaced by the string in .format()
-else:
-    print('a ({}) is not less than b ({})'.format(a, b))
-
-print("foo" if a < b else "bar");
